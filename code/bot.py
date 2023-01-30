0400e80925e4e64656d177736f008196af6f8fc8
Elia Albanese <eliaalbanese@icloud.com>
2023-01-30 13:54:11 +0100

    Ticket bot

---
+++ code/bot.py
@@ -0,0 +1,51 @@
+import configparser
+import math
+from datetime import datetime
+
+# import database as database
+from discord import (ApplicationContext, Bot, Embed,
+                     EmbedField, Member, Option, Permissions, Button, PartialEmoji)
+# from enums import PunishmentType
+from pytimeparse.timeparse import timeparse
+from view import SupportTicketCreateView, AddminTicketCreatView, ReportUserModal, SupportModal, BotProblemsModal
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+TOKEN = config.get('Bot', 'Token')
+DEBUG_GUILDS = None if config.get('Bot', 'DebugGuilds') == "" else list(
+    map(lambda id: int(id), config.get('Bot', 'DebugGuilds').split(',')))
+
+bot = Bot(debug_guild=DEBUG_GUILDS)
+# db = database.Database("bot.db")
+
+# db.create_tables()
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} is connected')
+
+
+# Ticket System
+@bot.slash_command(description="supportticket")
+async def suppticket(interaction: ApplicationContext):
+    embed = Embed(
+        title=f'Support Ticket',
+        description='If you need help, feel free to open one of the following tickets. A team member will be with you in no time.',
+    )
+    await interaction.respond("Created ticket embed", ephemeral=True)
+    await interaction.channel.send(embed=embed, view=SupportTicketCreateView())
+
+
+@bot.slash_command(description="adminticket")
+async def adminsuppticket(interaction: ApplicationContext):
+    embed = Embed(
+        title=f'Admin Ticket',
+        description='If you need help, feel free to open one of the following tickets. A team admin will be with you in no time.',
+    )
+    await interaction.respond("Created ticket embed", ephemeral=True)
+    await interaction.channel.send(embed=embed, view=AddminTicketCreatView())
+
+bot.run(TOKEN)
+# db.connection.close()
